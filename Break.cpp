a238117c919b74d8f2b9e5610a1ee50a82e414e6
Javohir-cryptic <javohirerkinovich@gmail.com>
2020-09-08 20:31:33 +0300

    Initial Commit

---
+++ Break.cpp
@@ -0,0 +1,26 @@
+﻿#include <stdio.h>
+#include <locale.h>
+#pragma warning(disable : 4996)
+
+int main()
+{
+    int A, B;
+    setlocale(LC_ALL, "Rus");
+    while (1) 
+
+    {
+        printf("\nВведите делимое и делитель:");
+        scanf("%d%d", &A, &B);
+
+        if (A == 0 && B == 0) break; 
+        if (B == 0) {
+
+            printf("Деление на ноль");
+            continue; 
+        }
+
+        printf("Частное %d, остаток %d", A / B, A % B);
+
+    }
+    getchar();
+}
